d764403a93743016060a2607d40eced8de98c5e3
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-02-04 16:33:56 +0530

    Updated practice_2.py with new changes

--- Chapter 7 Python/practice_2.py
+++ Chapter 7 Python/practice_2.py
@@ -1,1 +1,49 @@
+# Practice While loop
+#  1 Print Numbers from 1 to 10
+# count = 1
+# while count <=10:
+#     print(count)
+#     count += 1
 
+
+#  2 Sum of First N Natural Numbers
+
+# n = int(input("enter the number: "))
+# i = 1
+# total = 0
+# while i <= n:
+#     total  += i
+#     i += 1
+# print("sum of first", n , "natural numer: ", total)    
+
+# 3  Prime Number Checker
+
+# n = int(input("enter the number: "))
+# Is_prime = True
+# if n <= 1:
+#     Is_prime = False
+# else:
+#     i = 2
+#     while i <= n // 2:
+#         if n % i == 0:
+#             Is_prime =False
+#             break
+#         i += 1
+# if Is_prime:
+#     print(n, "is_prime")
+# else:
+#     print(n, "is not prime")          
+
+# 4 Print Message 3 Times
+# count = 0
+# while count < 3:
+#     print("My name is.....")
+#     count += 1
+
+# 5 Multiplication Table of a Number
+
+# number = int(input("enter the number: "))
+# i = 1
+# while i <=10:
+#     print(f"{number} x {i} = {number * i}")
+#     i += 1
